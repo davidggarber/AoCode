fb0b2071df18022b22e64cb17c6216828cf35873
David Garber <davidggarber@hotmail.com>
2022-12-02 21:12:31 -0800

    2022 day 3

--- aoc2022/Day3.cpp
+++ aoc2022/Day3.cpp
@@ -16,12 +16,21 @@ namespace Day3
     class Data
     {
     public:
+        vector<string> left;
+        vector<string> right;
+
         Data()
         {
             ifstream file(FileFromNamespace(__FUNCSIG__));
             if (file.is_open())
             {
-                string line;
+                while (!file.eof())
+                {
+                    string line;
+                    file >> line;
+                    left.push_back(line.substr(0, line.size() / 2));
+                    right.push_back(line.substr(line.size() / 2));
+                }
             }
         }
     };
@@ -29,13 +38,49 @@ namespace Day3
     size_t Part1()
     {
         Data data;
-        return 0;
+        size_t sum = 0;
+        for (int i = 0; i < data.left.size(); i++)
+        {
+            for (int a = 0; a < data.left[i].size(); a++)
+            {
+                char ch = data.left[i][a];
+                if (data.right[i].find(ch) < data.right[i].size())
+                {
+                    if (ch >= 'a')
+                        sum += (ch - 'a' + 1);
+                    else
+                        sum += (ch - 'A' + 27);
+                    break;
+                }
+            }
+        }
+        return sum;
     }
 
     size_t Part2()
     {
         Data data;
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < data.left.size(); i += 3)
+        {
+            string a = data.left[i] + data.right[i];
+            string b = data.left[i+1] + data.right[i+1];
+            string c = data.left[i+2] + data.right[i+2];
+            for (int j = 0; j < a.size(); j++)
+            {
+                char ch = a[j];
+                if (b.find(ch) < b.size() && c.find(ch) < c.size())
+                {
+                    if (ch >= 'a')
+                        sum += (ch - 'a' + 1);
+                    else
+                        sum += (ch - 'A' + 27);
+                    break;
+                }
+
+            }
+        }
+        return sum;
     }
 
     /// <summary>
--- aoc2022/aoc2022.cpp
+++ aoc2022/aoc2022.cpp
@@ -1,4 +1,4 @@
-#define DAY Day2
+#define DAY Day3
 
 // To simplify the code organization, each day only as a cpp file, not a .h file.
 // So we have to import it via "extern".
