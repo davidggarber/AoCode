4b393b5d91bdac262899bf7c2798dee2477c7129
David Garber <dgarber@microsoft.com>
2022-12-08 15:49:57 -0800

    2021 day 11

--- aoc2021/Day11.cpp
+++ aoc2021/Day11.cpp
@@ -17,7 +17,7 @@ namespace Day11
     class Data
     {
     public:
-        vector<string> doc;
+        vector<vector<int>> cave;
 
         Data()
         {
@@ -28,23 +28,97 @@ namespace Day11
                 while (!file.eof())
                 {
                     getline(file, line);
-                    doc.push_back(line);
+                    vector<int> row;
+                    for (int i = 0; i < line.size(); i++)
+                    {
+                        int n = line[i] - '0';
+                        row.push_back(n);
+                    }
+                    cave.push_back(row);
                 }
             }
         }
 
+        void Increment(int r, int c)
+        {
+            if (r < 0 || c < 0 || r >= cave.size() || c >= cave[r].size())
+                return;
+            cave[r][c] += 1;
+            if (cave[r][c] == 10)
+            {
+                Increment(r - 1, c-1);
+                Increment(r - 1, c);
+                Increment(r - 1, c+1);
+                Increment(r, c-1);
+                Increment(r, c+1);
+                Increment(r + 1, c-1);
+                Increment(r + 1, c);
+                Increment(r + 1, c+1);
+            }
+        }
+
+        bool Flash(int r, int c)
+        {
+            if (cave[r][c] >= 10)
+            {
+                cave[r][c] = 0;
+                return true;
+            }
+            return false;
+        }
     };
 
     int Part1()
     {
         Data data;
-        return 0;
+        int flashes = 0;
+        for (int round = 0; round < 100; round++)
+        {
+            for (int r = 0; r < data.cave.size(); r++)
+            {
+                for (int c = 0; c < data.cave[0].size(); c++)
+                {
+                    data.Increment(r, c);
+                }
+            }
+            for (int r = 0; r < data.cave.size(); r++)
+            {
+                for (int c = 0; c < data.cave[0].size(); c++)
+                {
+                    if (data.Flash(r, c))
+                        flashes++;
+                }
+            }
+        }
+        return flashes;
     }
 
     size_t Part2()
     {
         Data data;
-        return 0;
+        int flashes = 0;
+        int step = 0;
+        do
+        {
+            step++;  // first step is #1
+            flashes = 0;
+            for (int r = 0; r < data.cave.size(); r++)
+            {
+                for (int c = 0; c < data.cave[0].size(); c++)
+                {
+                    data.Increment(r, c);
+                }
+            }
+            for (int r = 0; r < data.cave.size(); r++)
+            {
+                for (int c = 0; c < data.cave[0].size(); c++)
+                {
+                    if (data.Flash(r, c))
+                        flashes++;
+                }
+            }
+        } while (flashes != data.cave.size() * data.cave[0].size());
+        return step;
     }
 
     /// <summary>
