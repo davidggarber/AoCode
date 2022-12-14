a569e61d1d09e71cc6eca56c3ad562555d114a56
David Garber <davidggarber@hotmail.com>
2022-12-13 22:43:31 -0800

    2022 day 10

--- aoc2022/Day10.cpp
+++ aoc2022/Day10.cpp
@@ -16,11 +16,33 @@ namespace Day10
     class Data
     {
     public:
+        vector<int> start;
+
         Data()
         {
+            int x = 1;
+            start.push_back(0);  // cycle 0 starts with no x, and ends initializing x=1
+            start.push_back(x);  // cycle 1 starts with x=1
+
             ifstream file(FileFromNamespace(__FUNCSIG__));
             if (file.is_open())
             {
+                while (!file.eof())
+                {
+                    string cmd;
+                    int val;
+                    file >> cmd;
+                    if (cmd == "noop")
+                        start.push_back(x);
+                    else
+                    {
+                        file >> val;
+                        // no change in first cycle
+                        start.push_back(x);  // start of 2nd cycle
+                        x += val;  // x changes at end of that cycle
+                        start.push_back(x);  // start of next cmd
+                    }
+                }
                 string line;
             }
         }
@@ -29,12 +51,26 @@ namespace Day10
     size_t Part1()
     {
         Data data;
-        return 0;
+        size_t sum = 0;
+        for (int i = 20; i < data.start.size(); i += 40)
+            sum += data.start[i] * i;
+        return sum;
     }
 
     size_t Part2()
     {
         Data data;
+        for (int i = 1; i < data.start.size(); i++)
+        {
+            int pos = ((i - 1) % 40);
+            if (pos == 0)
+                cout << "\n";
+            int x = data.start[i];
+            if (x >= pos - 1 && x <= pos + 1)
+                cout << "#";
+            else
+                cout << ".";
+        }
         return 0;
     }
 
