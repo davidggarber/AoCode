4cbd58480c1a4c3ca94b3a9b36453a899f8602ef
David Garber <davidggarber@hotmail.com>
2022-12-21 00:51:35 -0800

    2022 Day 13

--- aoc2022/Day13.cpp
+++ aoc2022/Day13.cpp
@@ -13,9 +13,111 @@ using namespace std;
 // https://adventofcode.com/2022/day/13
 namespace Day13
 {
+    class Packet
+    {
+    public:
+        string raw;
+
+        Packet(string r)
+        {
+            raw = r;
+        }
+
+        Packet(const Packet& p) = default;
+
+        const Packet& operator=(const Packet& p)
+        {
+            raw = p.raw;
+            return *this;
+        }
+
+        bool operator==(const Packet& p) const
+        {
+            return raw == p.raw;
+        }
+
+        bool IsListContent() const
+        {
+            int depth = 0;
+            for (auto it = raw.begin(); it != raw.end(); it++)
+            {
+                if (*it == '[')
+                    depth++;
+                else if (*it == ']')
+                    depth--;
+                else if (*it == ',' && depth == 0)
+                    return true;
+            }
+            if (depth != 0)
+                cout << "Unbalanced packet: " << raw << "\n";
+            return false;
+        }
+
+        bool IsSimple() const
+        {
+            return raw.size() == 0
+                || (!IsListContent() && raw[0] != '[');
+        }
+
+        bool IsListContainer() const
+        {
+            return !IsListContent() && raw[0] == '[';
+        }
+
+        vector<Packet> AsList() const
+        {
+            vector<Packet> list;
+            if (raw.size() == 0)
+                return list;
+            if (IsListContent())
+            {
+                // Current level is contents of a list
+                string s = "";
+                int depth = 0;
+                for (auto it = raw.begin(); it != raw.end(); it++)
+                {
+                    if (*it == ',' && depth == 0)
+                    {
+                        list.push_back(Packet(s));
+                        s = "";
+                        continue;
+                    }
+                    if (*it == '[')
+                        depth++;
+                    else if (*it == ']')
+                        depth--;
+                    s += *it;
+                }
+                list.push_back(s);
+            }
+            else if (IsListContainer())
+            {
+                // Current level is a list
+                string contents = raw.substr(1, raw.size() - 2);
+                if (contents.size() > 0)
+                    list.push_back(Packet(contents));
+            }
+            else
+            {
+                list.push_back(Packet(raw));
+            }
+            return list;
+        }
+
+        int AsValue() const
+        {
+            if (raw.size() == 0)
+                return -1;
+            return atoi(raw.c_str());
+        }
+    };
+
     class Data
     {
     public:
+        vector<Packet> left, right;
+        vector<Packet> sorted;
+
         Data()
         {
             ifstream file(FileFromNamespace(__FUNCSIG__));
@@ -23,23 +125,98 @@ namespace Day13
             {
                 while (!file.eof())
                 {
-                    string line;
-                    getline(file, line);
+                    string first, second, blank;
+                    getline(file, first);
+                    getline(file, second);
+                    if (!file.eof())
+                        getline(file, blank);
+                    left.push_back(Packet(first));
+                    right.push_back(Packet(second));
                 }
             }
+        }
+
+        int Insert(const Packet& packet)
+        {
+            int index = 0;
+            for (auto it = sorted.begin(); it != sorted.end(); it++)
+            {
+                bool correct = false;
+                index++;
+                if (TestCorrect(packet, *it, correct) && correct)
+                {
+                    sorted.insert(it, packet);
+                    return index;
+                }
+            }
+            sorted.insert(sorted.end(), packet);
+            return sorted.size();
+        }
+
+        bool TestCorrect(const Packet& f, const Packet& s, bool& correct)
+        {
+            if (f.IsSimple() && s.IsSimple())
+            {
+                auto fv = f.AsValue();
+                auto sv = s.AsValue();
+                correct = fv < sv;
+                return fv != sv;
+            }
+            else //if (f.IsListContainer() || s.IsListContainer())
+            {
+                auto ff = f.AsList();
+                auto ss = s.AsList();
+                for (int i = 0; i < ff.size(); i++)
+                {
+                    if (i >= ss.size())
+                    {
+                        correct = false;
+                        return true;
+                    }
+                    if (TestCorrect(ff[i], ss[i], correct))
+                        return true;
+                }
+                if (ff.size() < ss.size())
+                {
+                    correct = true;
+                    return true;
+                }
+                return false;  // identical lists
+            }
+        }
+
+        bool IsCorrect(int index)
+        {
+            Packet f = left[index];
+            Packet s = right[index];
+            bool correct = false;
+            return TestCorrect(f, s, correct) && correct;
         }
     };
 
     size_t Part1()
     {
         Data data;
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < data.left.size(); i++)
+        {
+            if (data.IsCorrect(i))
+                sum += i + 1;
+        }
+        return sum;
     }
 
     size_t Part2()
     {
         Data data;
-        return 0;
+        for (int i = 0; i < data.left.size(); i++)
+        {
+            data.Insert(data.left[i]);
+            data.Insert(data.right[i]);
+        }
+        int a = data.Insert(Packet("[[2]]"));
+        int b = data.Insert(Packet("[[6]]"));
+        return a * b;
     }
 
     /// <summary>
