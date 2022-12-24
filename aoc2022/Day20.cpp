184e2f561dbb375a2f08f586111b56c3a5d3ee72
David Garber <davidggarber@hotmail.com>
2022-12-23 22:04:44 -0800

    2022 Day 20, unsolved

--- aoc2022/Day20.cpp
+++ aoc2022/Day20.cpp
@@ -16,6 +16,9 @@ namespace Day20
     class Data
     {
     public:
+        vector<int> list;
+        vector<int> order;
+
         Data()
         {
             ifstream file(FileFromNamespace(__FUNCSIG__));
@@ -23,17 +26,43 @@ namespace Day20
             {
                 while (!file.eof())
                 {
-                    string line;
-                    getline(file, line);
+                    int n;
+                    file >> n;
+                    list.push_back(n);
+                    order.push_back(n);
                 }
             }
         }
     };
 
-    size_t Part1()
+    int Part1()
     {
         Data data;
-        return 0;
+        int size = data.order.size();
+        for (auto it = data.order.begin(); it != data.order.end(); it++)
+        {
+            auto lit = find(data.list.begin(), data.list.end(), *it);
+
+            int pos = lit - data.list.begin();
+            data.list.erase(lit);
+            pos += *it;
+            while (pos < 0)
+                pos += data.list.size();
+            pos %= data.list.size();
+            lit = data.list.begin() + pos;
+            data.list.insert(lit, *it);
+        }
+
+        int zPos = find(data.list.begin(), data.list.end(), 0) - data.list.begin();
+        int a = (zPos + 1000) % size;
+        int b = (zPos + 2000) % size;
+        int c = (zPos + 3000) % size;
+
+        int aa = data.list[a];
+        int bb = data.list[b];
+        int cc = data.list[c];
+
+        return aa + bb + cc;
     }
 
     size_t Part2()
