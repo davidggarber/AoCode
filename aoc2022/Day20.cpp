1fa0877bd7179cf97c8c90adaf6d0443041a35b2
David Garber <davidggarber@hotmail.com>
2022-12-31 17:21:16 -0800

    2022 Day 20

--- aoc2022/Day20.cpp
+++ aoc2022/Day20.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
+#include "Point.h"
 
 using namespace std;
 
@@ -16,21 +17,60 @@ namespace Day20
     class Data
     {
     public:
-        vector<int> list;
-        vector<int> order;
+        vector<Point> list;
 
-        Data()
+        Data(int64_t key = 1)
         {
             ifstream file(FileFromNamespace(__FUNCSIG__));
             if (file.is_open())
             {
+                int64_t order = 0;
                 while (!file.eof())
                 {
-                    int n;
+                    int64_t n;
                     file >> n;
-                    list.push_back(n);
-                    order.push_back(n);
+                    list.push_back(Point(n * key, order++));
                 }
+            }
+        }
+
+        int64_t FindOrder(int64_t order)
+        {
+            auto it = find_if(list.begin(), list.end(), [&order](const Point& p) { return p.y == order; });
+            return it - list.begin();
+        }
+
+        int64_t FindValue(int64_t value)
+        {
+            auto it = find_if(list.begin(), list.end(), [&value](const Point& p) { return p.x == value; });
+            return it - list.begin();
+        }
+
+        void Print()
+        {
+            for (auto it = list.begin(); it != list.end(); it++)
+            {
+                if (it != list.begin())
+                    cout << ", ";
+                cout << it->x;
+            }
+            cout << "\n";
+        }
+
+        void Shuffle()
+        {
+            int64_t gaps = list.size() - 1;
+            for (int64_t order = 0; order < list.size(); order++)
+            {
+                int64_t pos = FindOrder(order);
+                Point val = list[pos];
+                auto lit = list.begin() + pos;
+                list.erase(lit);
+                // pos is now the index of the item AFTER the removed item
+                pos += val.x;
+                pos = (((pos % gaps) + gaps) % gaps);
+                lit = list.begin() + pos;
+                list.insert(lit, val);
             }
         }
     };
@@ -38,37 +78,44 @@ namespace Day20
     int Part1()
     {
         Data data;
-        int size = data.order.size();
-        for (auto it = data.order.begin(); it != data.order.end(); it++)
-        {
-            auto lit = find(data.list.begin(), data.list.end(), *it);
+        int64_t size = data.list.size();
+        //data.Print();
+        data.Shuffle();
+        //data.Print();
 
-            int pos = lit - data.list.begin();
-            data.list.erase(lit);
-            pos += *it;
-            while (pos < 0)
-                pos += data.list.size();
-            pos %= data.list.size();
-            lit = data.list.begin() + pos;
-            data.list.insert(lit, *it);
-        }
+        int64_t zPos = data.FindValue(0);
+        int64_t a = (zPos + 1000) % size;
+        int64_t b = (zPos + 2000) % size;
+        int64_t c = (zPos + 3000) % size;
 
-        int zPos = find(data.list.begin(), data.list.end(), 0) - data.list.begin();
-        int a = (zPos + 1000) % size;
-        int b = (zPos + 2000) % size;
-        int c = (zPos + 3000) % size;
-
-        int aa = data.list[a];
-        int bb = data.list[b];
-        int cc = data.list[c];
+        int64_t aa = data.list[a].x;
+        int64_t bb = data.list[b].x;
+        int64_t cc = data.list[c].x;
 
         return aa + bb + cc;
     }
 
     size_t Part2()
     {
-        Data data;
-        return 0;
+        Data data(811589153);
+        int64_t size = data.list.size();
+        //data.Print();
+        for (int64_t i = 0; i < 10; i++)
+        {
+            data.Shuffle();
+            //data.Print();
+        }
+
+        int64_t zPos = data.FindValue(0);
+        int64_t a = (zPos + 1000) % size;
+        int64_t b = (zPos + 2000) % size;
+        int64_t c = (zPos + 3000) % size;
+
+        int64_t aa = data.list[a].x;
+        int64_t bb = data.list[b].x;
+        int64_t cc = data.list[c].x;
+
+        return aa + bb + cc;
     }
 
     /// <summary>
