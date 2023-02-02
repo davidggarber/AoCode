b0081b003049112066081a3a8bec81324aa082b0
David Garber <dgarber@microsoft.com>
2023-02-02 12:29:53 -0800

    2021 day 13

--- aoc2021/Day13.cpp
+++ aoc2021/Day13.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include "Stopwatch.h"
 #include <algorithm>
+#include "Point.h"
 
 using namespace std;
 
@@ -14,10 +15,18 @@ namespace Day13
 {
     auto data_file = "Data\\Day13.txt";
 
+    struct Fold
+    {
+        bool horz;
+        int at;
+    };
+
     class Data
     {
     public:
-        vector<string> doc;
+        vector<Point> points;
+        vector<Fold> folds;
+        int width, height;
 
         Data()
         {
@@ -28,23 +37,81 @@ namespace Day13
                 while (!file.eof())
                 {
                     getline(file, line);
-                    doc.push_back(line);
+                    if (line.size() == 0)
+                        break;
+                    points.push_back(Point(line));
+                }
+                while (!file.eof())
+                {
+                    getline(file, line);
+                    Fold fold;
+                    fold.horz = line[11] == 'x';
+                    fold.at = atoi(line.substr(13).c_str());
+                    folds.push_back(fold);
+                    if (fold.horz)
+                        width = fold.at;
+                    else
+                        height = fold.at;
                 }
             }
         }
 
     };
 
-    int Part1()
+    size_t Part1()
     {
         Data data;
-        return 0;
+        unordered_set<Point> set;
+        for (int i = 0; i < data.points.size(); i++)
+        {
+            Point pt = data.points[i];
+            auto it = data.folds.begin();
+            if (it->horz && pt.x > it->at)
+            {
+                pt.x = it->at * 2 - pt.x;
+            }
+            else if (!it->horz && pt.y > it->at)
+            {
+                pt.y = it->at * 2 - pt.y;
+            }
+            set.insert(pt);
+        }
+
+        return set.size();
     }
 
-    size_t Part2()
+    string Part2()
     {
         Data data;
-        return 0;
+        unordered_set<Point> set;
+        for (int i = 0; i < data.points.size(); i++)
+        {
+            Point pt = data.points[i];
+            for (auto it = data.folds.begin(); it != data.folds.end(); it++)
+            {
+                if (it->horz && pt.x > it->at)
+                {
+                    pt.x = it->at * 2 - pt.x;
+                }
+                else if (!it->horz && pt.y > it->at)
+                {
+                    pt.y = it->at * 2 - pt.y;
+                }
+            }
+            set.insert(pt);
+        }
+
+        string output(data.height * (data.width + 1), '.');
+        for (int y = 0; y < data.height; y++)
+            output[(y + 1) * (data.width + 1) - 1] = '\n';
+
+        for (auto it = set.begin(); it != set.end(); it++)
+        {
+            int offset = it->y * (data.width + 1) + it->x;
+            output = output.substr(0, offset) + '#' + output.substr(offset + 1);
+        }
+
+        return output;
     }
 
     /// <summary>
---
+++ aoc2021/Rect.h
@@ -0,0 +1,204 @@
+#pragma once
+
+#include <algorithm>
+#include "Point.h"
+
+class Rect
+{
+public:
+	int64_t left, top;
+	int64_t width, height;
+	Rect()
+	{
+		left, top, width, height = 0;
+	}
+	Rect(int64_t ll, int64_t tt, int64_t ww, int64_t hh)
+	{
+		left = ll;
+		top = tt;
+		width = ww;
+		height = hh;
+	}
+	Rect(const Point& a, const Point& b)
+	{
+		left = std::min(a.x, b.x);
+		top = std::min(a.y, b.y);
+		width = std::max(a.x, b.x) - left;
+		height = std::max(a.y, b.y) - top;
+	}
+	Rect(const Rect& rc)
+	{
+		left = rc.left;
+		top = rc.top;
+		width = rc.width;
+		height = rc.height;
+	}
+	const Rect& operator=(const Rect& rc)
+	{
+		left = rc.left;
+		top = rc.top;
+		width = rc.width;
+		height = rc.height;
+		return *this;
+	}
+	bool operator==(const Rect& rc) const
+	{
+		return left == rc.left && top == rc.top && width == rc.width && height == rc.height;
+	}
+	bool operator!=(const Rect& rc) const
+	{
+		return !operator==(rc);
+	}
+	int64_t Right() const
+	{
+		return left + width;
+	}
+	int64_t Bottom() const
+	{
+		return top + height;
+	}
+	bool IsEmpty() const
+	{
+		return width == 0 || height == 0;
+	}
+	bool operator<(const Rect& rc) const  // sort in reading order
+	{
+		return top < rc.top
+			|| (top == rc.top && left < rc.left)
+			|| (top == rc.top && left == rc.left && width < rc.width)
+			|| (top == rc.top && left == rc.left && width == rc.width && height < rc.height);
+	}
+	const Rect& operator|=(const Rect& rc)  // union
+	{
+		int64_t ll = std::min(left, rc.left);
+		int64_t tt = std::min(top, rc.top);
+		int64_t rr = std::max(Right(), rc.Right());
+		int64_t bb = std::max(Bottom(), rc.Bottom());
+		left = ll;
+		top = tt;
+		width = rr - ll;
+		height = bb - tt;
+		return *this;
+	}
+	Rect operator|(const Rect& rc) const
+	{
+		Rect u = *this;
+		return u |= rc;
+	}
+	const Rect& operator|=(const Point& pt)  // union
+	{
+		if (IsEmpty())
+		{
+			left = pt.x;
+			top = pt.y;
+			width = height = 1;
+		}
+		else
+		{
+			int64_t ll = std::min(left, pt.x);
+			int64_t tt = std::min(top, pt.y);
+			int64_t rr = std::max(Right(), pt.x + 1);
+			int64_t bb = std::max(Bottom(), pt.y + 1);
+			left = ll;
+			top = tt;
+			width = rr - ll;
+			height = bb - tt;
+		}
+		return *this;
+	}
+	Rect operator|(const Point& pt) const
+	{
+		Rect u = *this;
+		return u |= pt;
+	}
+	bool Intersects(const Rect& rc) const
+	{
+		return left < rc.Right()
+			&& Right() > rc.left
+			&& top < rc.Bottom()
+			&& Bottom() > rc.top;
+	}
+	bool Intersects(int64_t ll, int64_t tt, int64_t ww, int64_t hh) const
+	{
+		return left < (ll + ww)
+			&& Right() > ll
+			&& top < (tt + hh)
+			&& Bottom() > tt;
+	}
+	const Rect& operator&=(const Rect& rc)  // intersection
+	{
+		int64_t ll = std::max(left, rc.left);
+		int64_t tt = std::max(top, rc.top);
+		int64_t rr = std::min(Right(), rc.Right());
+		int64_t bb = std::min(Bottom(), rc.Bottom());
+		left = ll;
+		top = tt;
+		width = rr > ll ? (rr - ll) : 0;
+		height = bb > tt ? (bb - tt) : 0;
+		return *this;
+	}
+	Rect operator&(const Rect& rc) const
+	{
+		Rect inter = *this;
+		return inter &= rc;
+	}
+	const Rect& operator+=(const Point& pt)  // translate
+	{
+		left += pt.x;
+		top += pt.y;
+		return *this;
+	}
+	Rect operator+(const Point& pt) const
+	{
+		Rect r = *this;
+		return r += pt;
+	}
+	const Rect& operator-=(const Point& pt)  // translate
+	{
+		left -= pt.x;
+		top -= pt.y;
+		return *this;
+	}
+	Rect operator-(const Point& pt) const
+	{
+		Rect r = *this;
+		return r -= pt;
+	}
+
+	void Expand(const Point& dirs)
+	{
+		left -= dirs.x;
+		width += dirs.x * 2;
+		top -= dirs.y;
+		height += dirs.y * 2;
+	}
+
+	bool Contains(const Point& pt) const
+	{
+		return pt.x >= left && pt.x < Right()
+			&& pt.y >= top && pt.y < Bottom();
+	}
+
+	Point Wrap(const Point& pt) const
+	{
+		return Point(((((pt.x - left) % width) + width) % width) + left,
+			((((pt.y - top) % height) + height) % height) + top);
+	}
+
+	int64_t Area() const
+	{
+		return width * height;
+	}
+};
+
+template<>
+struct std::hash<Rect>
+{
+	size_t operator()(const Rect& r) const noexcept
+	{
+		return (size_t)(r.left << 48
+			^ r.top << 32
+			^ r.width << 16
+			^ r.height);
+	}
+};
