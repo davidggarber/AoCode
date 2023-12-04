6b81dd4e3c3bdda953965fe12b0701018611aa51
davidggarber <davidggarber@hotmail.com>
2023-12-03 21:55:11 -0800

    Day 5 prep

---
+++ aoc2023/Day5.js
@@ -0,0 +1,17 @@
+function solve1() {
+  var sum = 0;
+  for (var x of lines) {
+  }
+
+  print(1, sum);
+}
+
+
+function solve2() {
+  var sum = 0;
+  for (var x of lines) {
+    // trace(2, power);
+  }
+
+  // print(2, sum);
+}
