3e20d1d4887cb8c5246a8f70d8b8cd42b9624a08
davidggarber <davidggarber@hotmail.com>
2023-12-03 21:54:39 -0800

    Day 4

--- aoc2023/Day4.js
+++ aoc2023/Day4.js
@@ -1,6 +1,21 @@
 function solve1() {
   var sum = 0;
-  for (var x of lines) {
+  for (var line of lines) {
+    var parts = line.split(/\:|\|/);  // split on : or |
+    var wins = parts[1].match(/[^ ]+/g);  // split, clumping all non-spaces
+    var tix = ' ' + parts[2] + ' ';
+    var pts = 0;
+    var c = 0;
+    var d = ' ';
+    for (var w of wins) {
+      if (tix.match(' ' + w + ' ') != null) {
+        pts = pts == 0 ? 1 : (pts * 2);
+        c++;
+        d += w + ' ';
+      }
+    }
+    trace(1, c + ': ' + d);
+    sum += pts;
   }
 
   print(1, sum);
@@ -9,9 +24,26 @@ function solve1() {
 
 function solve2() {
   var sum = 0;
-  for (var x of lines) {
-    // trace(2, power);
+  var i = 0;
+  var dupes = new Array(lines.length).fill(0);
+  for (var line of lines) {
+    var parts = line.split(/\:|\|/);
+    var wins = parts[1].match(/[^ ]+/g);
+    var tix = ' ' + parts[2] + ' ';
+    var c = 0;
+    dupes[i]++;
+    for (var w of wins) {
+      if (tix.match(' ' + w + ' ') != null) {
+        c++;
+      }
+    }
+    for (; c > 0; c--) {
+      dupes[i + c] += dupes[i];
+    }
+    trace(2, dupes[i]);
+    sum += dupes[i];
+    i++;
   }
 
-  // print(2, sum);
+  print(2, sum);
 }
