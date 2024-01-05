233a7e18b9715fc13554041bbe52d9e39f6640d9
davidggarber <davidggarber@hotmail.com>
2024-01-05 00:32:52 -0800

    Day 24.2

--- aoc2023/Day24.js
+++ aoc2023/Day24.js
@@ -42,86 +42,178 @@ function intersect2d(a, b) {
   return [NaN, NaN];
 }
 
+
 /*
-  Given:
-    A: ax,ay,az + t(ix,iy,iz)
-    B: bx,by,bz + u(jx,jy,jz)
-    C: cx,cy,cz + v(kx,ky,kz)
-  ...
-  There exists a point on A, and a vector to a point on C (at a different time u!=t)
-  Continuing on, it must then hit E.
-    B: ax,ay,az + (u-t)(hx,hy,hz)
-    C: ax,ay,az + (v-t)(hx,hy,hz)
-    where t, u, v, hx, hy, hz are all unknown
+  Reviewing sample data, there are pairs of hail vectors with matching dx, dy, or dz.
+  The rock must traverse from one to the other in a whole number of seconds.
+  So the rock's dx/dy/dz comes from the subset of (shared delta) +/- (integer factors of starting point offsets)
 
-    bx+u*jx == ax+(u-t)hx
-    by+u*jy == ay+(u-t)hy
-    bz+u*jz == az+(u-t)hz
-    cx+v*kx == ax+(v-t)hx
-    cy+v*ky == ay+(v-t)hy
-    cz+v*kz == az+(v-t)hz
+  The most limiting case in my sample data: 
+    2 hail vectors have dy=25 and y=252498326441926
+      (but vectors' x and z don't otherwise intersect)
+      Since hail starting offset = 0, rock's dy = 25 +/- 0 = 25
+    dx and dy can be determined by intersections of possible options
   
-    hx = (bx-ax+u*jx)/(u-t) = (cx-ax+v*kx)/(v-t)
-    hy = (by-ay+u*jy)/(u-t) = (cy-ay+v*ky)/(v-t)
-    hz = (bz-az+u*jz)/(u-t) = (cz-az+v*kz)/(v-t)
-    Three unknowns: t, u, v, where t != u != v
-
-    Isolate v with x:
-      (v-t)*(bx-ax+u*jx) = (cx-ax+v*kx)*(u-t)
-      v*(bx-ax) - t*(bx-ax) + v*u*jx - t*u*jx = u*(cx-ax) - t*(cx-ax) + u*v*kx - t*v*kx
-      v*(bx-ax) + v*u*jx - u*v*kx + t*v*kx = u*(cx-ax) - t*(cx-ax) + t*(bx-ax) + t*u*jx 
-      v * ((bx-ax) + u*(jx-kx) + t*kx) = u*(cx-ax) - t*(cx-ax) + t*(bx-ax) + t*u*jx 
-      v = (u*(cx-ax) + t*(bx-cx) + t*u*jx) / ((bx-ax) + u*(jx-kx) + t*kx)
-
-    Isolate v with y:
-      v = (u*(cy-ay) + t*(by-cy) + t*u*jy) / ((by-ay) + u*(jy-ky) + t*ky)
-
-    Isolate u or t:
-      (u*(cx-ax) + t*(bx-cx) + t*u*jx) * ((by-ay) + u*(jy-ky) + t*ky)
-        = (u*(cy-ay) + t*(by-cy) + t*u*jy) * ((bx-ax) + u*(jx-kx) + t*kx)
-      u^2*(cx-ax)*(jy-ky) + u^2*t*jx*(jy-ky) + t*u*((bx-cx)*(jy-ky) + ky*(cx-ax) + jx*(by-ay)) + t^2*u*jx*ky + t^2*(bx-cx)*ky + u*(cx-ax)*(by-ay) + t*(bx-cx)*(bx-ax)
-        = u^2*(cy-ay)*(jx-kx) + u^2*t*jy*(jx-kx) + t*u*((by-cy)*(jx-kx) + kx*(cy-ay) + jy*(bx-ax)) + t^2*u*jy*kx + t^2*(by-cy)*kx + u*(cy-ay)*(bx-ax) + t*(by-cy)*(by-ay)
-      u^2*((cx-ax)*(jy-ky)-(cy-ay)*(jx-kx)) + u^2*t*(jx*(jy-ky)-jy*(jx-kx)) + t*u*(((bx-cx)*(jy-ky) + ky*(cx-ax) + jx*(by-ay)) - ((by-cy)*(jx-kx) + kx*(cy-ay) + jy*(bx-ax))) + t^2*u*jx*ky + t^2*(bx-cx)*ky + u*(cx-ax)*(by-ay) + t*(bx-cx)*(bx-ax)
-      
-    *** This would be much simpler if any of the subtractions came out as zero:
-        bx-cx, cy-ay, jx-kx, etc.
-      The goal is to get some cubic terms (i.e. u^2*t) to drop away
-    *** Similarly, if any vector component (i.e. ix) were zero, then the t*u partial component would be zero
-      So... find samples with zero components in offset vectors
-      then find repeated x/y/z elsewhere
-
-
-*/
-
+  Once we've established the rock's dx/dy/dz, take any pair of hail vectors to see where they must align.
+  Look back along that vector to get the starting point.
+ */
 
 function solve2() {
-  // find pairs of parallel rays
-  var hail = [];
-  var vecs = {};
-  var parallels = [];
+  var vecs = [];
   for (var line of lines) {
     var h = line.match(/[0-9\-]+/g);
     h = h.map(hh => parseInt(hh));
-    var v = h.slice(3, 6);
-    v[0] = 0;  // ignore z
-    var gcd = gcd3(v);
-    v = v.map(vv => vv / gcd);
-    if (v in vecs) {
-      parallels[v] = [vecs[v], h];
-    }
-    vecs[v] = h;
+    vecs.push(h);
   }
 
-  print(parallels.length);
+  var delta = [0,1,2].map(d => deltaFromSynchronizedVectors(vecs, d));
+
+  var start = startFromAlignment(vecs[0], vecs[1], delta);
+  trace('Start = ' + start.join(', '));
+  print(start[0] + start[1] + start[2]);
 }
 
-function gcd3(v) {
-  return gcd2(gcd2(v[0],v[1]),v[2]);
+function deltaFromSynchronizedVectors(vecs, axis) {
+  // Reduce to one axis and its delta
+  var simp = vecs.map(v => [v[axis],v[axis+3]]);
+  var rates = {};
+  for (var v of simp) {
+    if (!(v[1] in rates)) {
+      rates[v[1]] = [];
+    }
+    rates[v[1]].push(v[0]);
+  }
+
+  var deltas = [];  // [rate, [list of offsets]]
+
+  for (var r of Object.keys(rates)) {
+    if (rates[r].length > 1) {
+      // trace(rates[r].length + ' vectors all moving at ' + r);
+      var rr = rates[r].sort();
+      var diffs = [];
+      for (var i = 1; i < rates[r].length; i++) {
+        diffs.push(rr[i] - rr[i-1]);
+      }
+      var g = gcdN(diffs);
+      var facts = allFactors(g);
+      // trace('  gcd= ' + g);
+      deltas.push([parseInt(r), facts]);
+    }
+  }
+
+  var slopes = null;
+  for (var d of deltas) {
+    var s = {};
+    for (var f of d[1]) {
+      s[d[0] + f] = d[0] + f;
+      s[d[0] - f] = d[0] - f;
+    }
+    // trace('and ' + Object.values(s).join(', '));
+    slopes = intersectSets(slopes, s);
+    // trace('  = ' + Object.values(slopes).join(', '));
+  }
+
+  trace('Possible slopes: ' + Object.keys(slopes).join(', '));
+
+  // Should be a singleton
+  return Object.values(slopes)[0];
 }
+
+function intersectSets(a, b) {
+  if (a == null) {
+    return b;
+  }
+  var c = {};
+  for (var k of Object.keys(a)) {
+    if (k in b) {
+      c[k] = parseInt(k);
+    }
+  }
+  return c;
+}
+
+function gcdN(v) {
+  var prev = v[0];
+  for (var i = 1; i < v.length; i++) {
+    prev = gcd2(prev, v[i]);
+  }
+  return prev;
+}
+
 
 function gcd2(a,b) {
   if (!b) {
     return a;
   }
   return gcd2(b, a % b);
+}
+
+function primeFactors(n) {
+  var primes = []; // [1, n];
+  if (n < 0) {
+    n = -n;
+  }
+  var f = 2;
+  while (f * f <= n) {
+    if (n % f == 0) {
+      primes.push(f);
+      n /= f;
+      // factors.push(n);
+    }
+    else {
+      f += f == 2 ? 1 : 2;  // 2, 3, odds...
+    }
+  }
+  if (n != 1) {
+    primes.push(n);
+  }
+  return primes;
+}
+
+function allFactors(n) {
+  if (n == 0) {
+    return [0];
+  }
+  var primes = primeFactors(n);
+  var factors = {'1':1};
+  for (var i = 1; i < (1 << primes.length); i++) {
+    var f = 1;
+    for (var b = 0; b < primes.length; b++) {
+      if ((i & (1 << b)) == (1 << b)) {
+        f *= primes[b];
+      }
+    }
+    factors[f] = f;
+  }
+  return Object.values(factors);
+}
+
+function startFromAlignment(a, b, d) {
+  // Define a plane from the vectors a' and d, including point a.
+  // Where does ray b pass through that plane?
+
+  var plane = [a[4]*d[2] - a[5]*d[1], a[5]*d[0] - a[3]*d[2], a[3]*d[1] - a[4]*d[0]];
+  plane.push(a[0] * plane[0] + a[1] * plane[1] + a[2] * plane[2]);  // Ax + By + Cz = D
+
+  trace('Given hail ' + a.join(', '));
+  trace('  and rock vector ' + d.join(', '));
+  trace('  plane is ' + plane[0] + 'x + ' + plane[1] + 'y + ' + plane[2] + 'z = ' + plane[3]);
+
+  // Confirm for a at t=1 (a + a')
+  // var aa = [a[0] + a[3], a[1] + a[4], a[2] + a[5]];
+  // trace('D1= ' + (aa[0] * plane[0] + aa[1] * plane[1] + aa[2] * plane[2]));
+  // // Confirm for a + d
+  // aa = [a[0] + d[0], a[1] + d[1], a[2] + d[2]];
+  // trace('D2= ' + (aa[0] * plane[0] + aa[1] * plane[1] + aa[2] * plane[2]));
+
+
+  // A(bx + tbx') + B(by + tby') + C(bz + tbz') = D
+  // t = (D - Abx - Bby - Cbz) / (Abx' + Bby' + Cbz');
+  // REVIEW: why is round required? Javascript bit depth?
+  var t = Math.round((plane[3] - plane[0] * b[0] - plane[1] * b[1] - plane[2] * b[2])
+    / (plane[0] * b[3] + plane[1] * b[4] + plane[2] * b[5]));
+  var bt = [b[0] + t*b[3], b[1] + t*b[4], b[2] + t*b[5]];
+  trace('At time ' + t + ', hail ' + b.join(', ') + ' intersects the plane at ' + bt.join(','));
+  // trace('DB= ' + (bt[0] * plane[0] + bt[1] * plane[1] + bt[2] * plane[2]));
+  return [bt[0] - t*d[0], bt[1] - t*d[1], bt[2] - t*d[2]];
 }
